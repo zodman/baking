f75714c2d91217cbe134702a2f6da800e46949cb
Andres Vargas ( zodman ) <zodman@foresightlinux.org>
2009-03-20 17:01:38 -0600

    update

--- bake-remote.py
+++ bake-remote.py
@@ -18,19 +18,20 @@ result = commands.getoutput( "%(where)s build --quiet --no-watch  --context \"%(
 
 enums.notify("Packages Queue", ",".join(reps) )
 cmd["result"] = result
-enums.notify("Job: %s" % result,"")
+enums.notify("Job: %s" % result,",".join(reps))
 old_state = ""
 state=""
 while True:
     old_state = state
+    if "Built" in state:
+        commands.getoutput("%s commit %s"%( enums.where, job) )
+        enums.notify("Commit Package", "%s" % ",".join(reps))
+
     query = commands.getoutput("%(where)s q %(result)s " % cmd)
     job, state, timeago = enums.get_info(query)
     enums.notify("%s %s"% ( state, ",".join(reps)) , "Job: %s - %s    (%s)"%( job, state, timeago.lstrip()) )
     if old_state != state:
-        print state
-    if "Built" in state:
-        commands.getoutput("%s commit %s"%( where, job) )
-        enums.notify("Commit Package", "%s" % reps)
+        print "status: %s" % state
     if "Committed" in state or "Failed" in state :
         print "exiting ......"
         enums.notify("%s" % state, "%s"%  ",".join(reps) )
--- enums.py
+++ enums.py
@@ -1,12 +1,14 @@
-import pynotify, sys, re
+import pynotify, sys, re, os
 if not pynotify.init("Basic"):
     sys.exit(1)
 
-
-
+import gtk
+icon =gtk.gdk.pixbuf_new_from_file(
+    os.path.abspath(os.path.dirname(__file__))+'/64.png'
+)
 where = "ssh fdev.foresightlinux.org rmake"
 context="fl:2-devel"
-TIME = 60
+TIME = 30
 REGEX="(?P<job>\w*)\s+(?P<state>\w*)\s+(?P<time>\d*\s\w*\s\w*)\s+"
 
 def get_info(query):
@@ -14,6 +16,8 @@ def get_info(query):
     obj = re.compile(REGEX,  re.IGNORECASE| re.DOTALL)
     return obj.search(query).groups()
 
-def notify( summary, desc ):
-    print "%s \n %s \n=======" % ( summary, desc)
-    pynotify.Notification(summary, desc).show()
+def notify( summary, desc , warning = True):
+    print "%s - %s " % ( summary, desc)
+    notify =pynotify.Notification(summary, desc)
+    notify.set_icon_from_pixbuf(icon)
+    notify.show()
